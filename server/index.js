2c335aee964a76e75819e41ea8e2c3632040ba69
Tashfeen Chohan <chohantashfeen@gmail.com>
2024-09-10 23:21:01 +0500

    MySql connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
-const express = require("express")
-const app = express()
+require("dotenv").config()
+const express = require("express");
+const app = express();
+const db = require("./db");
 
-app.listen(3000, () => {
-  console.log("Server is listening.")
-})
+db.query("SELECT 1")
+  .then(() => {
+    console.log("MySql connected successfully!");
+    app.listen(3000, () => {
+      console.log("Server is listening.");
+    });
+  })
+  .catch((error) => console.log("MySql connection error : " + error));
