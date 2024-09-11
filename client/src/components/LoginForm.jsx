e87858c2da3ea56fdfe7a84a0a8f5155c6e1033c
AimaArshad <139466925+AimaArshad@users.noreply.github.com>
2024-09-12 00:05:53 +0500

    Create LoginForm.jsx

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,91 @@
+// src/components/LoginForm.jsx
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+//import '../styles.css'; // Import the CSS file
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   /* if (username.includes('-')) {
+      navigate('/student');
+    } else if (/^\d+$/.test(username)) { // Simple CNIC check (numeric only)
+      navigate('/administration');
+    } else {
+      alert('Invalid username');
+    }*/
+      if (username.includes('@')) {
+        navigate('/administration');
+      } else if (username.includes('-')) {
+        navigate('/student');
+      } else {
+        alert('Invalid username');
+      }
+  };
+
+
+
+  /*return (
+    <div className="container">
+      <div className="form-container">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input 
+              type="text" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required 
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input 
+              type="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};*/
+
+
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input 
+            type="text" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            required 
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input 
+            type="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            required 
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
