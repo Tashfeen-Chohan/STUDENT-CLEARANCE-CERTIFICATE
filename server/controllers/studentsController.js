04123a4891f4ac0d7c7d24856132c75a4bda3692
Tashfeen Chohan <chohantashfeen@gmail.com>
2024-09-11 09:56:58 +0500

    Student Login

--- server/controllers/studentsController.js
+++ server/controllers/studentsController.js
@@ -1,17 +1,17 @@
 const db = require("../db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // NEW STUDENT [STUDENT REGISTER]
-const newStudent = async (req, res) => {
+const registerStudent = async (req, res) => {
   const { roll_no, name, password, dept } = req.body;
   if (!roll_no || !name || !password || !dept) {
     return res.status(400).send({ message: "All fields are required!" });
   }
   try {
-    const [user] = await db.query(
-      "SELECT * FROM students WHERE roll_no = ?",
-      [roll_no]
-    );
+    const [user] = await db.query("SELECT * FROM students WHERE roll_no = ?", [
+      roll_no,
+    ]);
     if (user.length > 0) {
       return res.status(400).send({ message: "Student already exists!" });
     }
@@ -28,6 +28,34 @@ const newStudent = async (req, res) => {
   }
 };
 
+// LOGIN STUDENT
+const loginStudent = async (req, res) => {
+  const { roll_no, password } = req.body;
+  if (!roll_no || !password)
+    return res
+      .status(400)
+      .send({ message: "Please provide Roll No & Password" });
+  try {
+    const [user] = await db.execute(
+      "SELECT * FROM students WHERE roll_no = ?",
+      [roll_no]
+    );
+    if (user.length === 0) {
+      return res.status(400).send({ message: "Invalid Credentials!" });
+    }
+    const passMatched = await bcrypt.compare(password, user[0].password);
+    if (!passMatched)
+      return res.status(400).send({ message: "Invalid Credentials" });
+
+    const token = jwt.sign({ id: user[0].id }, process.env.JWT_SECRET);
+    res.status(200).send({ message: "Student login successfully!", token });
+  } catch (error) {
+    res.status(500).send({message: "Failed to Login Student"})
+    console.log(error.message)
+  }
+};
+
 module.exports = {
-  newStudent,
+  registerStudent,
+  loginStudent
 };
--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const controller = require("../controllers/studentsController")
 
-router.route("/register").post(controller.newStudent)
+router.route("/register").post(controller.registerStudent)
+router.route("/login").post(controller.loginStudent)
 
 module.exports = router;
